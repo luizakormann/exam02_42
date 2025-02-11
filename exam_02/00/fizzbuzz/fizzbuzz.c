45833f26d577edb1e18ccbcca58e3f1b650b960f
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2025-02-11 18:29:29 -0300

    rm .out & fizzbuzz OK

--- exam_02/00/fizzbuzz/fizzbuzz.c
+++ exam_02/00/fizzbuzz/fizzbuzz.c
@@ -5,24 +5,72 @@ int main(void)
     int num;
     char    *fizz;
     char    *buzz;
+    int     i;
+    int     n;
+    int     r;
 
     num = 1;
     fizz = "fizz";
     buzz = "buzz";
+    i = 0;
+    n = num;
+    r = 0;
     while (num < 101)
     {
         if (num % 3 == 0 && num % 5 == 0)
         {
-            write (1, &fizz, sizeof(fizz));
-            write (1, &buzz, sizeof(buzz));
+            while (fizz[i])
+            {
+                write (1, &fizz[i], 1);
+                i++;
+            }
+            i = 0;
+            while (buzz[i])
+            {
+                write (1, &buzz[i], 1);
+                i++;
+            }
+            i = 0;
+            write (1, "\n", 1);
         }
         else if (num % 3 == 0 && num % 5 != 0)
-            write (1, &fizz, sizeof(fizz));
+        {
+            while (fizz[i])
+            {
+                write (1, &fizz[i], 1);
+                i++;
+            }
+            i = 0;
+            write (1, "\n", 1);
+        }
         else if (num % 5 == 0 && num % 3 != 0)
-            write (1, &buzz, sizeof(buzz));
+        {
+            while (buzz[i])
+            {
+                write (1, &buzz[i], 1);
+                i++;
+            }
+            i = 0;
+            write (1, "\n", 1);
+        }
         else
-            write (1, &num, sizeof(num));
+        {
+            if (n >= 10)
+            {
+                r = (n % 10) + '0';
+                n = (n / 10) + '0';
+                write (1, &n, 1);
+                write (1, &r, 1);
+            }
+            else
+            {
+                n += '0';
+                write (1, &n, 1);
+            }
+            write (1, "\n", 1);
+        }
         num++;
+        n = num;
     }
     return (0);
 }
