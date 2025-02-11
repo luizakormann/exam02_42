d42fce1a57b8cfbcaeea0bb8686abd730f210f67
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2025-02-11 18:40:32 -0300

    first_word OK

--- exam_02/00/first_word/first_word.c
+++ exam_02/00/first_word/first_word.c
@@ -2,22 +2,24 @@
 
 int	main(int argc, char *argv[])
 {
-	char	*string;
+	char	*str;
 	int		i;
 
-	if (argc == 1)
+	if (argc != 2)
+	{
 		write (1, "\n", 1);
+		return (0);
+	}
 	else
 	{
 		i = 0;
-		string = argv[1];
-		while (string[i])
+		str = argv[1];
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			write (1, &string[i++], 1);
-			if (string[i] == 32 || string[i] == '\t')
-                                break ;
+			write (1, &str[i], 1);
+			i++;
 		}
-		write (1, "\n", 1);
 	}
+	write (1, "\n", 1);
 	return (0);
 }
